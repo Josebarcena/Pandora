b9830a6d4486436c97fb20ceb6a8d949cee43712
AlexLFab <72382170+AlexLFab@users.noreply.github.com>
2024-02-13 21:51:56 +0100

    Mi prueba de concepto
    No tiene gravedad ni salto de momento, comprobad que tal va

---
+++ Ejemplos/Prueba de Alex/level.py
@@ -0,0 +1,55 @@
+import pygame
+from settings import TILESIZE, WORLD_MAP
+from tile import Tile
+from player import Player
+from debug import debug
+
+class Level:
+    def __init__(self):
+        #get display surface
+        self.display_surface = pygame.display.get_surface()
+        #sprite group setup
+        self.visible_sprites = YSortCameraGroup()
+        self.obstacles_sprites = pygame.sprite.Group()
+
+        #sprite setup
+        self.create_map()
+
+    def create_map(self):
+        for row_index,row in enumerate(WORLD_MAP):
+            for col_index, col in enumerate(row):
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+                if col == 'x':
+                    Tile((x,y), [self.visible_sprites, self.obstacles_sprites])
+                elif col == 'p':
+                    self.player = Player((x,y), [self.visible_sprites], self.obstacles_sprites)
+
+
+    def run(self):
+        #update and draw the game
+        self.visible_sprites.custom_draw(self.player)
+        self.visible_sprites.update()
+      
+
+
+
+
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+
+        #general setup
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vector2()
+
+    def custom_draw(self, player):
+
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+
+        for sprite in self.sprites():
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image, offset_pos)
