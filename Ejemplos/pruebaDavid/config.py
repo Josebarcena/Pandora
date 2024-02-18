f81bac0fae85c03963cba56b47771840ff36fb76
dcuervopallin <92185265+dcuervopallin@users.noreply.github.com>
2024-02-18 16:27:33 +0100

    scroll lateral/vertical + gravedad y mecánica de salto

--- Ejemplos/pruebaDavid/config.py
+++ Ejemplos/pruebaDavid/config.py
@@ -1,30 +1,68 @@
+#DECLARACIÓN DE VARIABLES
+# Tamaño de la ventana
 WIN_WIDTH = 640
 WIN_HEIGHT = 480
+# Tamaño de los cubos usados y FPS que usará el reloj del programa
 TILESIZE = 32
 FPS = 60
+# Limites en el eje x e y (el limite de la izq es SCROLL_LIMIT_X y el de la derecha será WIN_WIDTH - SCROLL_LIMIT_X)
+SCROLL_LIMIT_X = 106
+SCROLL_LIMIT_Y = 80
 
+# Fuerza de la gravedad y numero de frames que dura el salto
+GRAVITY = 2
+FRAMES_JUMP = 30
+
+# Diferentes capas para el juego
 PLAYER_LAYER = 2
 BLOCK_LAYER = 1
-PLAYER_SPEED= 3
+PLAYER_SPEED = 3
 
 RED = (255, 0, 0)
 BLACK = (0, 0, 0)
 BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
 
+# Diferentes configuraciones del mapa a usar en las pruebas. Las B serán los bloques, los puntos se ignoran y la P será
+# la posición del jugador
 titlemap = [
-    'BBBBBBBBBBBBBBBBBBBB',
-    'B..................B',
-    'B..BBB.............B',
-    'B....B.............B',
-    'B..................B',
-    'B..................B',
-    'B....B.............B',
-    'B....B.............B',
-    'B....BBBB..BBB.....B',
-    'B............B.....B',
-    'B.......P....B.....B',
-    'B..........BBB.....B',
-    'B..................B',
-    'B..................B',
-    'BBBBBBBBBBBBBBBBBBBB',
+    'BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB',
+    'B......................................B',
+    'B......................................B',
+    'B......................................B',
+    'B..................................BBBBB',
+    'B.........................BBBBB.BBB....B',
+    'B........................B.............B',
+    'B.......................B..............B',
+    'B............BBBB..BBBBB...............B',
+    'B...........B..........................B',
+    'B..........B...........................B',
+    'B..P......B............................B',
+    'B........B.............................B',
+    'B.......B..............................B',
+    'BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB',
+]
+
+titlemap2 = [
+    'BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB',
+    'B......................................B',
+    'B......................................B',
+    'B...P..................................B',
+    'B......................................B',
+    'B......................................B',
+    'B......................................B',
+    'B......................................B',
+    'B..BBB.............BBBBBBBB..BBBBBB....B',
+    'B....B.................B..........B....B',
+    'B......................B..........B....B',
+    'B......................B...............B',
+    'B....B.................B......BB.......B',
+    'B....B.................BBBB...BB.......B',
+    'B....BBBB..BBB................BB.......B',
+    'B............B................BB.......B',
+    'B............B................BB.......B',
+    'B..........BBB..............BB..BB.....B',
+    'B...........................BB..BB.....B',
+    'B......................................B',
+    'BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB',
 ]
--- Ejemplos/pruebaDavid/sprites.py
+++ Ejemplos/pruebaDavid/sprites.py
@@ -3,43 +3,105 @@ from config import *
 import math
 import random
 
+# Definimos la clase Player en la que está implementada la mayoría de funcionalidad del código, debería de encapsularse y quitarle "responsabilidades"
 class Player(pygame.sprite.Sprite):
+    # Método con el que iniciamos el objeto, partimos de las coordenadas iniciales del juego y el propio juego
     def __init__(self, game, x, y):
+        # Iniciamos las variables del jugador para poder acceder a ellas más adelante
         self.game = game
         self._layer = PLAYER_LAYER
         self.groups = self.game.all_sprites
         pygame.sprite.Sprite.__init__(self, self.groups)
 
-        # Posiciones iniciales del cubo
+        # Posiciones iniciales del cubo y tamaño
         self.x = x * TILESIZE
         self.y = y * TILESIZE
         self.width = TILESIZE
         self.heigh = TILESIZE
 
+        # Variables auxiliares que nos ayudarán a actualizar la posición del personaje
         self.x_change = 0
         self.y_change = 0
 
-        # Para las animaciones
+        # Para las animaciones, sin implementar
         self.facing = 'lef'
 
-        # Crear la supperficie del personaje
+        # Para implementar la gravedad se hace uso de estás variables, que nos indican el estado del personaje:
+        # self.state-> 'normal' en caso de que no esté en medio de ninguna animación
+        #               'jumping' en caso de que esté saltando, se usa para evitar el doble salto entre otras cosas
+        # self.bool_air-> booleano que almacena si personaje se encuentra sobre el aire, True, o si está sobre una
+        #               plataforma, False
+        # self.jump_frames-> en ella almacenamos el numero de frames que ocupará la animación de salto durante
+        # la ejecución, se usará de contador
+        # Ya que empezamos con nuestro personaje en el aire, sin realizar el salto así es como se inicializan.
+        self.state = 'normal'
+        self.bool_air = True
+        self.jump_frames = 0
+
+        # Inicializamos el rect del Sprite, ya que es un objeto importado de la libreria debemos de inicializar el rect
+        # para poder usar estas librerias correctamente, véase en las colisiones
         self.image = pygame.Surface([self.width, self.heigh])
         self.image.fill(RED)
         self.rect = self.image.get_rect()
         self.rect.x = self.x
         self.rect.y = self.y
 
+    # Método en el que se actualiza el cubo
     def update(self):
         self.movement()
 
         self.rect.x += self.x_change
         self.collide_blocks('x')
+
+        self.check_state()
         self.rect.y += self.y_change
         self.collide_blocks('y')
+
+        self.screen_check()
 
         self.x_change = 0
         self.y_change = 0
 
+    # Según el estado en el que nos encontremos debemos de añadir a las variables auxiliares la fuerza de gravedad,
+    # positiva en caso de estar en un estado 'normal', lo que hará que el jugador "se pegue" al suelo, y negativa en
+    # caso de que el jugador haya saltado, haciendo que "se despegue" del suelo
+    def check_state(self):
+        if self.state == 'normal' and self.bool_air:
+            self.y_change += GRAVITY
+        elif self.state == 'jumping':
+            if self.jump_frames > 0:
+                self.jump_frames -= 1
+                self.bool_air = True
+                self.y_change -= GRAVITY
+            else:
+                self.state = 'normal'
+                self.bool_air = True
+
+
+    # Comprobamos la posición del jugador al final de la actualización de pantalla, si se sale de los márgenes
+    # establecidos, moveremos la lista de sprites que conforman el nivel en dirección contraria a la que va el jugador
+    # y a la velocidad del jugador
+    def screen_check(self):
+        if self.rect.x <= SCROLL_LIMIT_X:
+            self.rect.x = SCROLL_LIMIT_X
+            for sprite in self.game.all_sprites:
+                sprite.rect.x += PLAYER_SPEED
+        elif self.rect.x >= WIN_WIDTH - SCROLL_LIMIT_X:
+            self.rect.x = WIN_WIDTH - SCROLL_LIMIT_X
+            for sprite in self.game.all_sprites:
+                sprite.rect.x -= PLAYER_SPEED
+        if self.rect.y <= SCROLL_LIMIT_Y:
+            self.rect.y = SCROLL_LIMIT_Y
+            for sprite in self.game.all_sprites:
+                sprite.rect.y += PLAYER_SPEED
+        elif self.rect.y >= WIN_HEIGHT - SCROLL_LIMIT_Y:
+            self.rect.y = WIN_HEIGHT - SCROLL_LIMIT_Y
+            for sprite in self.game.all_sprites:
+                sprite.rect.y -= PLAYER_SPEED
+
+    # En este método capturamos las teclas pulsadas por el usuario y añadimos el movimiento necesario a las variables
+    # auxiliares x_change y y_change, implementé el salto con SPACE pero sigues pudiendo desplazarte libremente por el
+    # nivel con 'S' y 'D'
     def movement(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
@@ -54,7 +116,16 @@ class Player(pygame.sprite.Sprite):
         if keys[pygame.K_s]:
             self.y_change += PLAYER_SPEED
             self.facing = 'down'
+        if keys[pygame.K_SPACE]:
+            # Para evitar que se pueda saltar en el aire/ doble salto, el jugador dede de haber terminado con la
+            # animación de saltar y estar apoyado en una superficie, se inicializa a 30 el contador de frames
+            # para la animación de saltar
+            if self.state == 'normal' and self.bool_air == False:
+                self.state = 'jumping'
+                self.jump_frames = FRAMES_JUMP
 
+    # Se estudian las colisiones del jugador con los sprites almacenados en game.blocks, que son los distintos bloques
+    # con los que se crea el nivel
     def collide_blocks(self, direction):
         if direction == "x":
             hits = pygame.sprite.spritecollide(self, self.game.blocks, False)
@@ -68,24 +139,36 @@ class Player(pygame.sprite.Sprite):
             if hits:
                 if self.y_change > 0:
                     self.rect.y = hits[0].rect.top - self.rect.height
+                    if self.bool_air:
+                        self.bool_air = False
                 if self.y_change < 0:
                     self.rect.y = hits[0].rect.bottom
+            # En caso de que no se haya colisiones verticalmente y bool_air era falso en el anterior frame significa que
+            # debemos de aplicar la gravedad, el jugador se cayó de la plataforma
+            elif self.bool_air == False:
+                self.bool_air =True
 
+
+# Defino los diferentes bloques que formarán parte del nivel
 class Block(pygame.sprite.Sprite):
-    def __init__(self, game, x, y):
+    def __init__(self, game, x, y, color):
         self.game = game
         self._layer = BLOCK_LAYER
+        # Se añade el bloque a las distintas listas del juego
         self.groups = self.game.all_sprites, self.game.blocks
         pygame.sprite.Sprite.__init__(self, self.groups)
 
+        # Colocamos el bloque según las posición que se nos indica y le damos el size oportuno
         self.x = x * TILESIZE
         self.y = y * TILESIZE
         self.width = TILESIZE
         self.height = TILESIZE
 
+        # Añadimos color y superficio al bloque
         self.image = pygame.Surface((self.width, self.height))
-        self.image.fill(BLUE)
+        self.image.fill(color)
 
+        # Inicializamos el rect del Sprite
         self.rect = self.image.get_rect()
         self.rect.x = self.x
         self.rect.y = self.y
