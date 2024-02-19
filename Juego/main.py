680a7b1f197055509d40c52aa214f589c8d9f6d8
Josebarcena <Jose.barcena@udc.es>
2024-02-19 21:15:39 +0100

    Menu inicial

--- Juego/main.py
+++ Juego/main.py
@@ -5,6 +5,27 @@ ANCHO = 1080
 ALTO = 720
 FASE = 1
 
+
+class Menu_principal:
+
+    
+
+    def __init__(self, pantalla):
+        self.fondo = pygame.image.load("Juego/Imagenes/bg.png")
+        self.pantalla = pantalla
+        self.fuente = pygame.font.SysFont("Barron", 40)
+        self.color_fuente = (255, 255, 255)
+
+    def dibujar_texto(self, text, x, y):
+        img = self.fuente.render(text, True, self.color_fuente)
+        pantalla.blit(img, (x,y))
+    
+    def dibujar_fondo(self):
+        self.pantalla.blit(self.fondo, (0,0))
+        self.dibujar_texto("Pulsa ESPACIO para empezar", ANCHO/(2.75), ALTO/1.25)
+    
+    
+
 if __name__ == '__main__': #Funcion necesaria para definir el main
 
     # Inicializar pygame
@@ -13,14 +34,20 @@ if __name__ == '__main__': #Funcion necesaria para definir el main
     pantalla = pygame.display.set_mode((ANCHO, ALTO), 0, 32)
     reloj = pygame.time.Clock()
 
-    fase = Fase()
+    menu = Menu_principal(pantalla)
+
     
     while True:
 
-        tempo = reloj.tick(60)
-        if (fase.eventos(pygame.event.get())):
-            pygame.quit()
-            sys.exit()
+        tempo = reloj.tick(120)
 
-        fase.dibujar(pantalla)
+        menu.dibujar_fondo()   
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            
+
         pygame.display.flip()
+
