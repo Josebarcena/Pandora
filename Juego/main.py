89d87244a70be57d20b1b96dbb9719033b23337b
Josebarcena <Jose.barcena@udc.es>
2024-02-19 19:39:57 +0100

    main creado

---
+++ Juego/main.py
@@ -0,0 +1,26 @@
+import pygame
+import sys
+
+ANCHO = 720
+ALTO = 640
+FASE = 1
+
+if __name__ == '__main__': #Funcion necesaria para definir el main
+
+    # Inicializar pygame
+    pygame.init()
+
+    pantalla = pygame.display.set_mode((ANCHO, ALTO), 0, 32)
+    reloj = pygame.time.Clock()
+
+    fase = Fase()
+    
+    while True:
+
+        tempo = reloj.tick(60)
+        if (fase.eventos(pygame.event.get())):
+            pygame.quit()
+            sys.exit()
+
+        fase.dibujar(pantalla)
+        pygame.display.flip()
