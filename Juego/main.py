e875caac39b97d69237846688057db797c2add1d
Josebarcena <Jose.barcena@udc.es>
2024-02-19 22:01:44 +0100

    estructurado en su propio archivo

--- Juego/main.py
+++ Juego/main.py
@@ -1,42 +1,11 @@
 import pygame
 import sys
-from pygame import mixer
+import main_menu
 
 ANCHO = 1080
 ALTO = 720
 FASE = 0
 
-
-class Menu_principal:
-
-    def __init__(self, pantalla):
-        self.fondo = pygame.image.load("Juego/Imagenes/bg.png")
-        self.pantalla = pantalla
-        self.fuente = pygame.font.SysFont("Barron", 40)
-        self.color_fuente = (255, 255, 255)
-        mixer.init()
-        mixer.music.load('Juego/Sonidos/intro.mp3')
-        mixer.music.set_volume(0.2)
-        mixer.music.play()
-
-    def dibujar_texto(self, text, x, y):
-        img = self.fuente.render(text, True, self.color_fuente)
-        pantalla.blit(img, (x,y))
-    
-    def dibujar_fondo(self):
-        self.pantalla.blit(self.fondo, (0,0))
-        self.dibujar_texto("Pulsa ESPACIO para empezar", ANCHO/(2.75), ALTO/1.25)
-    
-    def eventos(self,lista_eventos):
-        for evento in lista_eventos:
-            if evento.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-
-        teclasPulsadas = pygame.key.get_pressed()
-        if teclasPulsadas[pygame.K_SPACE]:
-            mixer.music.stop()
-            return True 
 
 if __name__ == '__main__': #Funcion necesaria para definir el main
 
@@ -46,13 +15,10 @@ if __name__ == '__main__': #Funcion necesaria para definir el main
     pantalla = pygame.display.set_mode((ANCHO, ALTO), 0, 32)
     reloj = pygame.time.Clock()
 
-    menu = Menu_principal(pantalla)
+    menu = main_menu.Menu_principal(pantalla)
 
     
     while True:
-
-        tempo = reloj.tick(120)
-
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -60,10 +26,13 @@ if __name__ == '__main__': #Funcion necesaria para definir el main
                 sys.exit()
 
         if(FASE == 0):
+            tempo = reloj.tick(30)
             menu.dibujar_fondo()   
             if (menu.eventos(pygame.event.get())):
                 FASE = 1   
         if(FASE == 1):
+            tempo = reloj.tick(60)
             print(FASE)
+            
         pygame.display.flip()
 
---
+++ Juego/main_menu.py
@@ -0,0 +1,47 @@
+import pygame
+import sys
+from pygame import mixer
+
+ANCHO = 1080
+ALTO = 720
+FASE = 0
+
+
+class Menu_principal:
+
+    def __init__(self, pantalla): # constructor de la clase donde tambien se inicializa la musica
+        self.fondo = pygame.image.load("Juego/Imagenes/bg.png")
+        self.pantalla = pantalla
+        self.fuente = pygame.font.SysFont("Barron", 40)
+        self.color_fuente = (255, 255, 255)
+        self.frame = 1
+
+        mixer.init()
+        mixer.music.load('Juego/Sonidos/intro.mp3')
+        mixer.music.set_volume(0.2)
+        mixer.music.play()
+
+    def dibujar_texto(self, text, x, y): # escribimos el texto pasado por parametro con la fuente del menu y color que cambia de opacidad por frames
+        img = self.fuente.render(text, True, self.color_fuente)
+        img.set_alpha(255/(self.frame/35))
+        self.frame+=1
+        if (self.frame == 60):
+                self.frame = 1
+        self.pantalla.blit(img, (x,y))
+        
+    
+    def dibujar_fondo(self): # dibujamos el fondo con el texto encima
+        self.pantalla.blit(self.fondo, (0,0))
+        self.dibujar_texto("Pulsa ESPACIO para empezar", ANCHO/(2.75), ALTO/1.25)
+    
+
+    def eventos(self,lista_eventos): # controlamos eventos por si el jugador sale o si inicia el juego
+        for evento in lista_eventos:
+            if evento.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        teclasPulsadas = pygame.key.get_pressed()
+        if teclasPulsadas[pygame.K_SPACE]:
+            mixer.music.stop()
+            return True 
