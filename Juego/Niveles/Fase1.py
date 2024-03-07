984a88c4c7dd5c657c2cf4ed408652c5ece25987
Josebarcena <Jose.barcena@udc.es>
2024-03-07 17:43:11 +0100

    barra de vida

--- Juego/Niveles/Fase1.py
+++ Juego/Niveles/Fase1.py
@@ -118,6 +118,7 @@ class Fase1(Fase): #Clase para el primer nivel del juego
     def draw(self, surface): #la funcion que llama en bucle game para pintar cada frame la fase
         surface.fill((123,211,247))
         sprites = self.all_sprites
+        self.player.viewers.draw()
         sprites.update()
         self.screen_check(sprites, self.player, self.stage.sprites()[0])
         self.visible_sprites.draw(surface)
