4fcf2df7af1c1fa45192d6f423a20c8731ff5e50
AlexLFab <alejandro.lopez.fabeiro@udc.es>
2024-03-10 17:57:58 +0100

    fase 2

---
+++ Juego/Recursos/Fases/1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="1" tilewidth="16" tileheight="16" tilecount="5600" columns="100">
+ <image source="SET1_PNG/1.jpg" width="1600" height="900"/>
+</tileset>
